0143b941900d8877fc1ffd3bbe7ca03d116c029d
Arvid Morelid <arvid.morelid@hotmail.com>
2024-03-19 13:53:14 +0100

    Update sample application

--- application/src/main.c
+++ application/src/main.c
@@ -1,13 +1,19 @@
-#include "msp430.h"
-#include "msp430fr5994.h"
+#include <msp430.h>
 
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
-    P1DIR |= BIT0;              // Set P1.0 to output direction
-    P1OUT &= ~BIT0;             // Set the LED off
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
 
-    while(1) {
-        P1OUT ^= BIT0;           // Toggle the LED
-        __delay_cycles(100000); // Delay for a while
+    // Configure GPIO
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+
+    while(1)
+    {
+        P1OUT ^= BIT0;                      // Toggle LED
+        __delay_cycles(500000);
     }
 }
