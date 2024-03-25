18883baf4bbcd47cf4f12f11e177e1e8dedae4f6
Arvid Morelid <arvid.morelid@hotmail.com>
2024-03-25 20:55:11 +0100

    Add button-ISR for invoking update later

--- src/main.c
+++ src/main.c
@@ -1,27 +1,52 @@
+
 #include <msp430.h>
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
 
-    // Configure GPIO
-    P1DIR |= BIT0;  // Set P1.0 to output direction
-    P1OUT |= BIT0;  // Set P1.0 to high
-
     // Disable the GPIO power-on default high-impedance mode to activate
     // previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
 
-    TA0CCTL0 = CCIE;                        // TACCR0 interrupt enabled
-    TA0CCR0 = 30000;
-    TA0CTL = TASSEL__SMCLK | MC__UP;        // SMCLK, UP mode
+    // Configure GPIO
+    // LED (P1.0)
+    P1OUT &= 0;  // Set P1.0 to high 
+    P1DIR |= BIT0;  // Set P1.0 to output direction
 
-    __bis_SR_register(LPM0_bits + GIE);     // Enter LPM0 w/ interrupt
-    __no_operation();                       // For debugger
+    // Button P5.5
+    P5REN |= BIT5;  // Enable pull-up resistor on P5.6
+    P5OUT |= BIT5;   // Set P5.5 to high
+
+    // Enable interrupts on P5.5
+    P5IE |= BIT5;   // Enable interrupt
+    P5IES |= BIT5;  // high-to-low transition
+    P5IFG &= 0;     // Clear flags
+
+    _BIS_SR(GIE);
+
+    long i = 0;
+    while(1) {
+        if (i == 20000) {
+            P1OUT ^= BIT0;                 // Toggle P1.0 using exclusive-OR
+            i = 0;
+        }
+        i++;
+    }
 }
 
-// Timer0_A0 interrupt service routine: toggles the P1.0 LED
-void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer0_A0_ISR (void)
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=PORT5_VECTOR
+__interrupt void Port_5(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(PORT5_VECTOR))) Port_5 (void)
+#else
+#error Compiler not supported!
+#endif
 {
-    P1OUT ^= BIT0;
+    if (P5IFG & BIT5) {
+        P5IFG &= ~BIT5;
+        P1OUT ^= BIT0; // Toggle LED
+    }
+    P5IFG |= 0;
 }
